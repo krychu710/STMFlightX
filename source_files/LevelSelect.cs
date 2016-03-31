27fbe096085bf4d6e899519fdf0e3e2357f2650f
fj2016 <kfigiel.projekty@gmail.com>
2016-03-31 18:22:12 +0200

    New_Files
    New game files were added. It will responsible for plane steering
    physics and menu.

---
+++ source_files/LevelSelect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+	public void LvlSelect(int level){
+		SceneManager.LoadScene(level);
+	}
+	public void Exit(){
+		Application.Quit();
+	}
+}
